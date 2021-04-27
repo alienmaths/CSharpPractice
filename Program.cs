2482f40dbbacb1e914ff3d481742b3ad52d37861
My Name <thesame@email.com>
2021-04-27 10:55:19 +0530

    Leetcode

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public static string stringReverse(string s)  //string reverse function
+    {
+        string s1 = "";
+        int num = s.Length - 1;
+
+        while (num >= 0)
+        {
+            s1 = s1 + s[num];
+            num--;
+        }
+        return s1;
+    }
+
+    public string LongestPalindrome(string s)
+    {
+        var list1 = new List<string>();
+        var st = "";
+
+        if (s.Length == 0 || s.Length == 1)
+            return s;
+        
+        else if (s.Length == 2)
+        {
+            if (s == Solution.stringReverse(s))
+                return s;
+            else
+            {
+                var ch = Char.ToString(s[0]);
+                return ch;
+            }
+        }
+        
+        else
+        {
+            for (int j = 0; j < s.Length; j++)
+            {
+                string empstr = "";
+                empstr = empstr + s[j];
+                for (int i = j + 1; i < s.Length; i++)
+                {
+                    empstr = empstr + s[i];
+                    if (empstr == Solution.stringReverse(empstr))
+                    {
+                        list1.Add(empstr);
+                    }
+                }
+            }
+            if (list1.Count == 0)
+            {
+                var ch = Char.ToString(s[0]);
+                return ch;
+            }
+            else
+            { 
+                var maxi = 0;
+                foreach (var j in list1)
+                {
+                    if (maxi < j.Length)
+                    {
+                        maxi = j.Length;
+                        st = j;
+                    }
+                }
+            }
+        }
+        return st;
+
+    }
+}
